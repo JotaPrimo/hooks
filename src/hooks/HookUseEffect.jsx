9227c86fa098e0f32c158f9fcfdbdccbe1239b25
JotaPrimo <gestald118@gmail.com>
2023-08-08 22:57:57 -0400

    167. Cleanup no useEffect

--- src/hooks/HookUseEffect.jsx
+++ src/hooks/HookUseEffect.jsx
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const HookUseEffect = () => {
   // useEffect, sem depencias, precisa forçar re-rendização do componente
   // para ser executado
 
-  useEffect(() => {
-    console.log("Estou sendo executado");
-  });
+//   useEffect(() => {
+//     console.log("Estou sendo executado");
+//   });
 
-  const [number, setNumber] = useState(1);
+//   const [number, setNumber] = useState(1);
 
-  const handleClickForChange = () => {
-    setNumber(number + 1);
-  };
+//   const handleClickForChange = () => {
+//     setNumber(number + 1);
+//   };
 
   // use effect apenas uma vez
   // deixar array de depencias vazio
   // 2 - array de dependências vazio
-  useEffect(() => {
-    console.log("Serei executado apenas uma vez!");
-  }, []);
+//   useEffect(() => {
+//     console.log("Serei executado apenas uma vez!");
+//   }, []);
 
   // 3 - item no array de dependências
-  const [anotherNumber, setAnotherNumber] = useState(0);
+//   const [anotherNumber, setAnotherNumber] = useState(0);
 
-  useEffect(() => {
-    console.log("Sou executado apenas quando anotherNumber muda!");
-  }, [anotherNumber]);
+//   useEffect(() => {
+//     console.log("Sou executado apenas quando anotherNumber muda!");
+//   }, [anotherNumber]);
+
+const [anotherNumber, setAnotherNumber] = useState(1)
+
+// useEffect(() => {
+//     const timer  = setTimeout(() => {
+//         console.log("Hello word");
+//     }, 2000);
+    
+//     return () => clearTimeout(timer)
+// }, [anotherNumber])
 
   return (
     <div>
       <h2>HookUseEffect</h2>
-      <p>Number: {number}</p>
-      <button onClick={handleClickForChange}>Alterar Valor</button>
+      <p>Number</p>
+      <button>Alterar Valor</button>
       <hr />
-      <p>Another number</p>
-      <button onClick={setAnotherNumber(anotherNumber + 1)}>
-        Alterar Valor
-      </button>
+      <p>Another number</p>     
     </div>
   );
 };
